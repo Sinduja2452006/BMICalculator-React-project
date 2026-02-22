6b4d5f4bc8ea65fb196f627a7ffaf62e6126329b
Sinduja2452006 <sinduja.senthil24@gmail.com>
2026-02-22 15:50:29 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BMICalculator from "./component/BMICalculator";
+import Result from "./component/Result";
 import "./App.css";
-import Car from "./vehicle/Car";
-import Bike from "./vehicle/Bike";
-import Truck from "./vehicle/Truck";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <h1>Vehicle Gallerys</h1>
-
-        <nav className="navbar">
-          <Link to="/car" className="nav-button">Car</Link>
-          <Link to="/bike" className="nav-button">Bike</Link>
-          <Link to="/truck" className="nav-button">Truck</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/car" element={<Car />} />
-          <Route path="/bike" element={<Bike />} />
-          <Route path="/truck" element={<Truck />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BMICalculator />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
